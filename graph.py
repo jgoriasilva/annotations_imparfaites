5109809cfbf26e15fa6754079e2d961f966d0c0d
Joao Antonio Goria Silva <jgoriasilva@gmail.com>
2020-10-17 16:38:03 +0200

    after training with train_quit

--- graph.py
+++ graph.py
@@ -2,30 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-train_type = input('train type? ')
+train_type = 'oubli'
+# train_type = input('train type? ')
 distortion_log = open(os.path.join('logs','distortion','distortion_'+train_type+'.log'),'r')
 jaccard_log = open(os.path.join('logs','jaccard','jaccard_'+train_type+'.log'),'r')
 distortion_data = []
 jaccard_data = []
 
-if train_type == 'oubli':
-	total_run = int(input('How many sequential runs? '))
-
 for line in distortion_log:
 	distortion_data.append(float(line.split(' ')[-1]))
+
+'''
 if train_type == 'oubli':
 	x = [item for item in distortion_data for _ in range(total_run)]
 	x = np.array(x)
 else:
 	x = np.array(distortion_data)
+'''
 
 for line in jaccard_log:
-	line_split = line.split(' ')
-	if(line_split[2] == 'test'):
-		jaccard_data.append(float(line.split(' ')[-1]))
-y = np.array(jaccard_data)
+	line = line.split(' ')
+	if(line[2] == 'test'):
+		jaccard_data.append([int(line[-4])/5, int(line[-2]), float(line[-1])])
+data = np.array(jaccard_data)
+
+data_0 = []
+data_1 = []
+data_2 = []
+data_3 = []
+
+for line in data:
+	line[0] = distortion_data[int(line[0])]
+	if line[1] == 0:
+		data_0.append([line[0],line[2]])
+	if line[1] == 1:
+		data_1.append([line[0],line[2]])
+	if line[1] == 2:
+		data_2.append([line[0],line[2]])
+	if line[1] == 3:
+		data_3.append([line[0],line[2]])
+
+data_0 = np.array(data_0)
+data_1 = np.array(data_1)
+data_2 = np.array(data_2)
+data_3 = np.array(data_3)
+
+print(data)
+
+print(data_0)
+print(data_1)
+print(data_2)
+print(data_3)
 
 
+plt.figure()
+plt.plot(data_0[:,0],data_0[:,1],'o',label='run 0')
+plt.plot(data_1[:,0],data_1[:,1],'o',label='run 1')
+plt.plot(data_2[:,0],data_2[:,1],'o',label='run 2')
+plt.plot(data_3[:,0],data_3[:,1],'o',label='run 3')
+plt.xlabel('Jaccard between ground truth labels and labels given to the network')
+plt.ylabel('Jaccard between ground truth labels and outputs of the network')
+plt.title('Relation between imperfect labeling and network performance')
+plt.xlim(0)
+plt.ylim(0)
+plt.legend()
+plt.grid()
+plt.savefig(os.path.join('images','graph_'+train_type+'.png'))
+plt.show()
+
+
+'''
 # Scatter plot
 plt.figure()
 if train_type == 'oubli':
@@ -34,7 +80,6 @@ if train_type == 'oubli':
 else:
 	plt.plot(x, y, 'o')
 
-'''
 # Curve fitting
 deg = int(input('Degree of polynomial fit: '))
 coef = np.polyfit(x, y, deg)
@@ -51,14 +96,7 @@ if deg == 1:
 	plt.plot(x, poly(x), label='y={:.2f}x+{:.2f}'.format(coef[0], coef[1]) + '; r2 = {:.2f}'.format(r2))
 elif deg == 2:
 	plt.plot(x, poly(x), label='y={:.2f}x^2+{:.2f}x+{:.2f}'.format(coef[0], coef[1], coef[2]) + '; r2 = {:.2f}'.format(r2))
+
 '''
 
-plt.xlabel('Jaccard between ground truth labels and labels given to the network')
-plt.ylabel('Jaccard between ground truth labels and outputs of the network')
-plt.title('Relation between imperfect labeling and network performance')
-plt.xlim(0)
-plt.ylim(0)
-plt.legend()
-plt.grid()
-plt.savefig(os.path.join('images','graph_'+train_type+'.png'))
-plt.show()
+
